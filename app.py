e16204949b5c9f0dca926f5e44e36e1003d73179
Maciej <macglowacki@gmail.com>
2026-05-13 10:01:50 +0200

    Dodano endpointy delete i put

--- app.py
+++ app.py
@@ -20,3 +20,19 @@ def pobierz_zadania():
 def nowe_zadanie(nazwa: str, status: str):
     dodaj_zadanie(nazwa, status)
     return {"message": f"Dodano zadnie: {nazwa}"}
+
+
+@app.delete("/zadania/{id}")
+def usun(id: int):
+    if not znajdz_zadanie(id):
+        return {"error": "Nie ma zadania o takim id"}
+    usun_zadanie(id)
+    return {"message": f"Zadanie {id} usunięte"}
+
+
+@app.put("/zadania/{id}")
+def zmien_status(id: int, status: str):
+    if not znajdz_zadanie(id):
+        return {"error": "Nie ma zadania o takim id"}
+    zmien_status_zadania(status, id)
+    return {"message": f"Status zadania {id} zmieniony na {status}"}
